a60611129c7a02377d0ec8f6a7e374cf7232090c
siddhant gore <siddhantgore111@gmail.com>
2021-10-15 15:47:07 +0530

    version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const userAdmin = {
+    email: "admin@gmail.com",
+    passwd: "admin1234"
+  }
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+  }
+  const Logout = () => {
+    console.log("Logout");
+  }
+
+  const Submit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !== "") ? (
+        <div className="welcome">
+          <h1>
+            Welcome , <spann>{user.name}</spann>
+          </h1>
+        </div>
+      ) :
+        <LoginForm Login={Login} error={error}/>
+      }
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,34 @@
+import React ,{useState} from 'react'
+
+function LoginForm(props) {
+
+    const [details, setDetails] = useState({ name: "", email: "", password: "" })
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        props.Login(details);
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-inner">
+                <h1>Login</h1>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" id="name" onChange={e=>setDetails({...details,name:e.target.value})}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" id="email"  onChange={e=>setDetails({...details,email:e.target.value})} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password"  onChange={e=>setDetails({...details,password:e.target.value})} />
+                </div>
+                <input type="submit" value="Log In" />
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
