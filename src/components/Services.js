4d7d40dab3d1d0dcf5cf703014f42f7ad1aa6752
siddhant gore <siddhantgore111@gmail.com>
2021-11-04 22:01:54 +0530

    updated

---
+++ src/components/Services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div className="Services">
+            <h1>This is Services Page</h1>
+            
+        </div>
+    )
+}
+
+export default Services
